f172daf8eab27d4938f3bbdc2f05649486b3100b
root <root@Lenovogroult>
2024-10-12 14:36:06 +0200

    ajout des commentaires

--- header.h
+++ header.h
@@ -1,22 +1,22 @@
 #ifndef HEADER_H
 #define HEADER_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdio.h> //Librairie utile
+#include <stdlib.h>//Librairie utile
+#include <time.h>//Librairie utile
 
-typedef struct {
+typedef struct { //Declaration de la structure pour localiser le joueur et son mouvement
     int x;
     int y;
     char mouvement;
 } PositionJoueur;
 
-typedef struct {
+typedef struct { //Declaration de la structure pour localiser la boite
     int x;
     int y;
 } PositionBoite;
 
-typedef struct {
+typedef struct { //Declaration de la structure pour localiser l'emplacement
     int x;
     int y;
 } PositionEntre;
